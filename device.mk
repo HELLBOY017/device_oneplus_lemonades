561d6cb0cc4e851d8a6b8283228527f8e490d123
chandu078 <chandu078@users.noreply.github.com>
2022-07-25 20:25:19 +0530

    lemonades: overlay: Override default settings global device name - Also set default hotspot ssid for OnePlus 8T
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: HELLBOY017 <abhaygill017@gmail.com>

--- device.mk
+++ device.mk
@@ -31,7 +31,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-zep
     
 PRODUCT_PACKAGES += \
-    OnePlus8TPowerProfileOverlay
+    OnePlus8TPowerProfileOverlay \
+    OnePlus8TWifiOverlay \
+    OnePlus8TSettingsProviderOverlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
